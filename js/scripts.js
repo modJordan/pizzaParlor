a21e147af70f4cd1f48da21486d0a3e71b75fa01
modjordan <mod.jordan@icloud.com>
2023-07-13 20:44:10 -0700

    WIP connecting Business logic to UI logic to get correct TDD output.

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,43 @@ function Order(size, toppings) {
 }
 
 Order.prototype.selectSize = function (size) {
-  size = document.querySelector("input[name=size]:checked").ariaValueMax;
-  return size
+  this.size = size;
+  console.log(size);
 }
 
 
 Order.prototype.addToppings = function (topping) {
-  topping = [document.querySelectorAll("input[name=toppings]:checked")]
+  topping = topping;
   return topping
 }
+
+//UI Logic
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const sizeSelector = document.querySelector("input[name=size]:checked").value;
+  const toppingsSelected = document.querySelector("form#checkbox-form input[name=toppings]:checked")
+  console.log(sizeSelector, toppingsSelected)
+  order.selectSize();
+  order.addToppings();
+}
+
+window.addEventListener("load", function () {
+  const form = document.querySelector("form#radio-form");
+  form.addEventListener("submit", handleFormSubmission);
+});
+
+window.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let sizeOutput = document.getElementById("radio-form");
+  let toppingOutput = document.getElementById("checkbox-form");
+  const sizeSelector = document.querySelector("input[name=size]:checked").value;
+  const toppingsSelected = document.querySelectorAll("form#checkbox-form input[name=toppings]:checked");
+
+  const order = new Order(sizeSelector, toppingsSelected);
+  order.selectSize();
+  order.addToppings();
+
+  sizeOutput.innerHTML = order.size;
+  toppingOutput.innerHTML = order.toppings;
+});
