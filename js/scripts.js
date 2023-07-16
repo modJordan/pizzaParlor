d9ba20db9472507bc5b33e8478cb7dd36b7b9c8d
modjordan <mod.jordan@icloud.com>
2023-07-15 19:55:20 -0700

    WIP Ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,22 @@
 
 function Order(size, toppings) {
   this.size = size;
-  this.toppings = toppings || [];
+  this.toppings = toppings;
 }
 
 Order.prototype.selectSize = function (size) {
   this.size = size
 }
 
-Order.prototype.toppingArray = function () {
-  for (let i = 0; i < arguments.length; i++) {
-    this.toppings.push(arguments[i]);
-  }
+Order.prototype.toppingArray = function (topping) {
+  this.toppings = topping
 }
 
 Order.prototype.addToppings = function (topping) {
   if (!Array.isArray(this.toppings)) {
     this.toppings = [];
   }
-  this.toppings = this.toppings.concat(topping);
-  return this.toppings;
+  this.toppings.push(topping);
 }
 
 
@@ -29,22 +26,42 @@ Order.prototype.addToppings = function (topping) {
 function handleFormSubmission(e) {
   e.preventDefault();
   const sizeSelector = document.querySelector("input[name=size]:checked").value;
-  const toppingsSelected = Array.from(document.querySelector("form#checkbox-form input[name=toppings]:checked"));
-  console.log(toppingsSelected);
+  const toppingsSelected = Array.from(document.querySelectorAll("form#checkbox-form input[name=toppings]:checked")).map(input => input.value);
 
   const order = new Order(sizeSelector, toppingsSelected);
   order.selectSize(sizeSelector);
-  toppingArray = []
-  toppingsSelected.toString.forEach(function (topping) {
-    order.addToppings(topping);
-    push.toppingArray(topping)
-  });
+
 
   const sizeOutput = document.getElementById("sizeInfo");
   const toppingOutput = document.getElementById("toppingInfo");
 
-  sizeOutput.innerHTML = order.size;
-  toppingOutput.innerHTML = order.toppings.join(", ");
+  sizeOutput.textContent = order.size;
+  toppingOutput.textContent = order.toppings.join(", ")
+
+  let outputBox = document.getElementById("pizzaPrice");
+  outputBox.removeAttribute("hidden");
+
+  function calculatePrice(size, toppings) {
+    let basePrice;
+    if (size === "small") {
+      basePrice = 10;
+    } else if (size === "medium") {
+      basePrice = 12;
+    } else if (size === "large") {
+      basePrice = 14;
+    } else {
+      return 0;
+    }
+
+    const toppingPrice = 1.5;
+    const toppingsCount = toppings.length;
+    const totalPrice = basePrice + (toppingPrice * toppingsCount);
+    return totalPrice.toFixed(2);
+  }
+
+  // let totalPrice = calculatePrice(order.size, order.toppings);
+  // let totalSpan = document.querySelector(".total");
+  // totalSpan.textContent = totalPrice;
 }
 
 window.addEventListener("load", function () {
@@ -60,17 +77,16 @@ window.addEventListener("submit", function (e) {
 
   const order = new Order(sizeSelector, toppingsSelected);
   order.selectSize(sizeSelector);
-  toppingsSelected.forEach(function (topping) {
-    order.addToppings(topping);
-  })
+  order.toppingArray(toppingsSelected);
 
   let sizeOutput = document.getElementById("sizeInfo");
   let toppingOutput = document.getElementById("toppingInfo");
 
-  sizeOutput.innerHTML = order.size;
-  toppingOutput.innerHTML = order.toppings.join(", ");
-  let outputBox = document.getElementById("pizzaPrice");
+  let output = document.getElementById("pizzaPrice");
+  output.removeAttribute("hidden");
 
-  outputBox.removeAttribute("hidden");
+  sizeOutput.textContent = order.size;
+  toppingOutput.textContent = order.toppings;
+
 });
 
